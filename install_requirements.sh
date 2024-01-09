118f5a30be62bc655b14b42c103f4c698acfce26
panoslouk <loukouzaspanos@gmail.com>
2024-01-09 21:24:42 +0200

    add public_ip feature

--- install_requirements.sh
+++ install_requirements.sh
@@ -1,4 +1,4 @@
 
 apt update && sudo apt upgrade -y
-apt install python3-pip
+sudo apt install python3-pip
 pip3 install paho-mqtt && pip3 install psutil && pip3 install pytest-shutil && pip3 install netifaces
